758f31bc66280b4df30e56bbbf653c2980eb64d1
Duhyeok Ko <kdha5@naver.com>
2020-03-22 19:09:25 +0900

    Set meta tag minimally

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const IndexPage = () => {
-  return <h1>Hello, world!</h1>
+  return (
+    <>
+      <Helmet titleTemplate="%s: Esprim">
+        <html lang="ko" />
+        <meta charSet="utf-8" />
+        <title>Hello, world</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      </Helmet>
+      <h1>Hello, world!</h1>
+    </>
+  )
 }
 
 export default IndexPage
