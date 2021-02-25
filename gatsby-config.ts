9e46c04382c0d76c1f7f5208a98d1bdbe04459c5
Duhyeok Ko <duhyeok.ko@gmail.com>
2021-02-26 00:01:45 +0900

    Add mdx for posting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,42 @@ import { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata,
-  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-tsconfig-paths'],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/data/mdx/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/data/mdx`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              exclude: 'Table of Contents',
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: 'table-of-contents',
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tsconfig-paths`,
+  ],
 }
 
 export default config
